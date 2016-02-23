2acdc5ea017964b1c13be42b8a77d661607ec835
Dawid Malinowski <dawidmalina@gmail.com>
2016-02-23 11:46:40 +0100

    Update db_converter.py

--- db_converter.py
+++ db_converter.py
@@ -84,7 +84,7 @@ def parse(input_filename, output_filename):
             # Inserting data into a table?
             elif line.startswith("INSERT INTO"):
                 if current_table in [ 'JQUARTZ_FIRED_TRIGGERS', 'JQUARTZ_JOB_DETAILS', 'JQUARTZ_TRIGGERS', 'OS_CURRENTSTEP', 'OS_CURRENTSTEP_PREV', 'OS_HISTORYSTEP', 'OS_HISTORYSTEP_PREV' ]:
-                    output.write(line.split('VALUES')[0] + " " + line.split('VALUES')[1].encode("utf8").replace("'0000-00-00 00:00:00'", "NULL") + "\n")
+                    output.write(re.sub('"','',line.split('VALUES')[0]) + " " + line.split('VALUES')[1].encode("utf8").replace("'0000-00-00 00:00:00'", "NULL") + "\n")
                 else:
                     output.write(line.encode("utf8").replace("'0000-00-00 00:00:00'", "NULL") + "\n")
                 num_inserts += 1
